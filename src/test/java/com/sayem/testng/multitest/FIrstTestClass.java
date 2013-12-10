b98deb9f0c3108f394705fc220bd1bd8d23e7916
Syed Sayem <syed@hotmail.com>
2013-12-09 22:36:53 -0500

    Added Multitest example

---
+++ src/test/java/com/sayem/testng/multitest/FIrstTestClass.java
@@ -0,0 +1,11 @@
+package com.sayem.testng.multitest;
+
+import org.testng.annotations.Test;
+
+public class FirstTestClass {
+
+    @Test
+    public void firstTest(){
+        System.out.println("First test method");
+    }
+}
---
+++ src/test/java/com/sayem/testng/multitest/SecondTestClass.java
@@ -0,0 +1,11 @@
+package com.sayem.testng.multitest;
+
+import org.testng.annotations.Test;
+
+public class SecondTestClass {
+
+    @Test
+    public void secondTest(){
+        System.out.println("Second test method");
+    }
+}
