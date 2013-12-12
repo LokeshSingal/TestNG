e0822e9a088a6ee38b45ebb783ab0338278f59e1
Syed Sayem <syed@hotmail.com>
2013-12-12 09:56:09 -0500

    sample project

--- src/test/java/com/sayem/testng/sampleproject/secondpackage/FirstTestClass.java
+++ src/test/java/com/sayem/testng/sampleproject/secondpackage/FirstTestClass.java
@@ -1,4 +1,16 @@
 package com.sayem.testng.sampleproject.secondpackage;
 
+import org.testng.annotations.Test;
+
 public class FirstTestClass {
+
+    @Test
+    public void firstTest(){
+        System.out.println("First test method");
+    }
+
+    @Test
+    public void secondTest(){
+        System.out.println("Second test method");
+    }
 }
