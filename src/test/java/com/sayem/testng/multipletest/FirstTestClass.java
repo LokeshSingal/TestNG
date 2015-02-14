2a061ba35f551e4871b2e83c3a70a13cfec7fd93
Syed Sayem <syed@sayem.org>
2015-02-14 18:28:56 -0500

    Added Second TestNG Test

---
+++ src/test/java/com/sayem/testng/multipletest/FirstTestClass.java
@@ -0,0 +1,11 @@
+package com.sayem.testng.multipletest;
+
+import org.testng.annotations.Test;
+
+public class FirstTestClass {
+    
+    @Test
+    public void firstTest(){
+        System.out.println("First Test");
+    }
+}
---
+++ src/test/java/com/sayem/testng/multipletest/SecondTestClass.java
@@ -0,0 +1,12 @@
+package com.sayem.testng.multipletest;
+
+import org.testng.annotations.Test;
+
+public class SecondTestClass {
+    
+    @Test
+    public void secondTest(){
+        System.out.println("Second test method");
+        
+    }
+}
